d0ca4712b88fa5c8a0f7ee4713be9448ca1155ea
fred <domaine75@gmail.com>
2023-08-31 00:02:49 +0200

    clean code

--- Datatables.ServerSide/Data/ApplicationDbContext.cs
+++ Datatables.ServerSide/Data/ApplicationDbContext.cs
@@ -1,17 +1,13 @@
 ﻿using Datatables.ServerSide.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Datatables.ServerSide.Data
 {
-    public class ApplicationDbContext : DbContext
+  public class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
-        public DbSet<Customer> Customers { get; set; }
     }
+    public DbSet<Customer> Customers { get; set; }
+  }
 }
